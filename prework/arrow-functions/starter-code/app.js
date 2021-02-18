866b303b8882c30c73fe1765d17763d5d56226b8
JamesShreckengost <shreckthegent@gmail.com>
2021-02-17 23:08:06 -0800

    arrow functions

--- prework/arrow-functions/starter-code/app.js
+++ prework/arrow-functions/starter-code/app.js
@@ -8,7 +8,7 @@ const theOldWay = function(course) {
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // console.log('The old way:', theOldWay('Code 301'));
-
+// push
 
 // STEP 2
 // We can refactor our first function to use an arrow function.
